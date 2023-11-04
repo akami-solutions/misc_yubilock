0199d0bc3fdf768a713baaf2525576b505d64f8e
SlushUwU <rology@satowa-network.eu>
2023-11-04 15:43:16 +0100

    Updated: `Added ServiceBase Runner`

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using YubiLock_Service;
 //System.Diagnostics.Debugger.Launch();
+/*
 IHost host = Host.CreateDefaultBuilder(args)
     .UseWindowsService(options =>
     {
@@ -8,8 +9,18 @@ IHost host = Host.CreateDefaultBuilder(args)
     })
     .ConfigureServices(services =>
     {
-        services.AddHostedService<Worker>();
+        services.AddHostedService<Worker>0();
     })
     .Build();
 
 await host.RunAsync();
+*/
+
+internal class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine(@"YubiLock Worker is not able to work in a normal Command Line.\nPlease start the Service normally or re-execute the Installer.");
+        System.ServiceProcess.ServiceBase.Run(new Worker());
+    }
+}
